03144d50df890f5399ae216f911e512844394d25
MasaharuTakeda <158536889+MasaharuTakeda@users.noreply.github.com>
2024-03-20 10:16:47 +0900

    Add files via upload

---
+++ login_script.js
@@ -0,0 +1,14 @@
+document.querySelector('button[type="submit"]').addEventListener('click', function() {
+  var passwordInput = document.querySelector('input[name="password"]');
+  var password = passwordInput.value;
+
+  // パスワードのチェック
+  if (password === 'toyota') { 
+      //alert('ログイン成功！');
+      window.location.href = 'index.html'; // index.htmlにリダイレクト
+  } else {
+      alert('パスワードが間違っています。もう一度お試しください。');
+      passwordInput.value = ''; // パスワードをクリア
+      passwordInput.focus(); // パスワード入力フィールドにフォーカスを戻す
+  }
+});
